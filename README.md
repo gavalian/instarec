d1d07efe3a44935173656d7a6d2da1a2fcfc86cb
gavalian <gavalian@jlab.org>
2025-02-03 15:36:28 -0500

    updated the README

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # instarec
-Track Reconstruction of AI
+Track Reconstruction with AI
+
+# Usage
+
+The reconstruction works on EVIO files, The code in class RunApp.java processes the file (raw EVIO) and creates a hybrid file with EVIO and H5 containers.
+Then the reconstruction code must be run on the produced HIPO-5 file by downloading coatjava branch "iss-442":
+
+```
+prompt> git clone -b iss-442 https://github.com/JeffersonLab/coatjava.git
+prompt> cd coatjava
+prompt> ./build-coatjava.sh
+prompt> ./coatjava/bin/decoder -i input.hipo -o output.hipo
+prompt> ./coatjava/bin/recon-util -i output.hipo -o rec_output.hipo -y etc/services/data-cv.yaml -n 1000
+```
+
+At this point the file will contain the hybrid HIPO-5 and reconstrcuted HIPO data.
+
+## Reading the output
+
+Here is an example of how to read InstaRec Banks from the hybrid reconstructed HIPO-5 data:
+
+```
+HipoReader r = new HipoReader("infile.h5");
+Bank[] b = r.getBanks("TimeBasedTrkg::TBTracks");
+
+Event e = new Event();
+
+while(r.next(e)){
+    e.read(b);
+    b[0].show();
+    Leaf leaf = e.readLeaf(1,12,32000,1);
+    leaf.print();
+}
+```
+
+The readLeaf() method provides the container ids (1,12) which is the same for all H5 leafs, then the leaf ids to read, in the example it reads tracks leaf containing identified tracks by AI.
+
+## Profiling
+
+A very lightweight approach to look at the number of objects per class is Class Histogram.
+
+Just generate class histogram by jmap -histo <PID>.
+
+Use -XX:+PrintClassHistogramBeforeFullGC, -XX:+PrintClassHistogramAfterFullGC to see class histograms when your stop-the-world pause occurs. You can compare the snapshot before/after to see which class instances get collected during the stop-the-world pause.
+
--- src/main/java/j4np/instarec/workers/RunApp.java
+++ src/main/java/j4np/instarec/workers/RunApp.java
@@ -4,15 +4,21 @@
  */
 package j4np.instarec.workers;
 
+import j4np.clas12.decoder.Clas12DecoderService;
+import j4np.clas12.decoder.Clas12FitterService;
+import j4np.clas12.decoder.Clas12TranslateService;
 import j4np.data.base.DataActor;
 import j4np.data.base.DataActorStream;
 import j4np.data.base.DataFrame;
 import j4np.data.base.DataWorker;
 import j4np.hipo5.data.Event;
+import j4np.hipo5.io.HipoChain;
 import j4np.hipo5.io.HipoReader;
 import j4np.hipo5.io.HipoWriter;
 import j4np.instarec.core.DriftChamberWorker;
+import j4np.instarec.core.Hipo2Hipo4Worker;
 import j4np.instarec.core.TrackFinderWorker;
+import j4np.utils.FileUtils;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -40,39 +46,52 @@ public class RunApp {
         return actors;
     }
     
+    
     public static void main(String[] args){
         
         //String file = "/Users/gavalian/Work/DataSpace/decoded/clas_006595.evio.00625-00629_DC.hipo";
         //String file = "/Users/tyson/data_repo/trigger_data/rgd/018326/run_18326_3.h5";
-        String file = "/Users/tyson/data_repo/trigger_data/sims/claspyth_train/clasdis_62.hipo";
+        //String file = "/Users/tyson/data_repo/trigger_data/sims/claspyth_train/clasdis_62.hipo";
+        String file = "/Users/gavalian/Work/DataSpace/INSTAREC_LDRD/evio/clas_005197.evio.00000.h5";
         HipoReader r = new HipoReader(file);
         
+        List<String> files = FileUtils.dir("/Users/gavalian/Work/DataSpace/evio/clas_005197", "*h5");
+        List<String> files2 = new ArrayList<>();
+        
+        for(int i = 0; i < 2; i++) files2.add(files.get(i));
         HipoWriter w = HipoWriter.create("w.h5", r);
+        HipoChain  chain = new HipoChain(files2);
         
         DataActorStream stream = new DataActorStream();
         
-        stream.setSource(r).setSync(w);
+        //stream.setSource(chain).setSync(w);
 
         String pathToClusterFinder = "etc/networks/clusterfinder/cf";
         String pathToElPID = "etc/networks/ElPID/ElPID";
-        double threshold=0.1;        
-        stream.setSource(r).setSync(w);
+        double threshold=0.1;
+        stream.setSource(chain).setSync(w);
         
                 
         ConverterWorker   convert = new ConverterWorker();
+        Clas12DecoderService decoder = new Clas12DecoderService();
+        Clas12FitterService   fitter = new Clas12FitterService();
+        Clas12TranslateService trans = new Clas12TranslateService();
         DriftChamberWorker  dcwrk = new DriftChamberWorker();
         TrackFinderWorker  finder = new TrackFinderWorker();
         ClusterFinderWorkerECAL  ecalfinder = new ClusterFinderWorkerECAL(pathToClusterFinder);
         ClusterFinderWorkerFTOF  ftoffinder = new ClusterFinderWorkerFTOF(pathToClusterFinder);
         ElPIDWorker elPID  = new ElPIDWorker(pathToElPID,threshold);
         
-        finder.initNetworks();
+        Hipo2Hipo4Worker h2h4 = new Hipo2Hipo4Worker(420*1024);
         
-        List<DataWorker>  workers = Arrays.asList(convert,dcwrk, finder,ecalfinder,ftoffinder,elPID);
+        finder.initNetworks();
+        decoder.setKeepEvio(true);
+        trans.setKeepEvio(true);
+        List<DataWorker>  workers = Arrays.asList(decoder,fitter,trans,dcwrk,finder,ecalfinder,ftoffinder,elPID,h2h4);//,ftoffinder,elPID);
         //List<DataWorker>  workers = Arrays.asList(convert,dcwrk, finder);
         
         List<DataActor>   actors = RunApp.createActors(6, 128, workers);
-        
+        for(DataActor a: actors) a.setBenchmark(1);
         stream.addActor(actors);//.addActor(convert2);//.addActor(convert3).addActor(convert4);
         
         stream.run();
