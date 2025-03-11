67d0d85fe415bb2dae44f0da76088ceebbc8924c
gavalian <gavalian@jlab.org>
2025-03-11 12:46:08 -0400

    some minor modifications

--- src/main/java/j4np/instarec/workers/RunApp.java
+++ src/main/java/j4np/instarec/workers/RunApp.java
@@ -13,10 +13,15 @@ import j4np.hipo5.io.HipoReader;
 import j4np.hipo5.io.HipoWriter;
 import j4np.instarec.core.DriftChamberWorker;
 import j4np.instarec.core.TrackFinderWorker;
+import j4np.utils.io.OptionParser;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import j4np.clas12.decoder.Clas12DecoderService;
+import j4np.clas12.decoder.Clas12FitterService;
+import j4np.clas12.decoder.Clas12TranslateService;
+import j4np.instarec.core.Hipo2Hipo4Worker;
 /**
  *
  * @author gavalian
@@ -42,37 +47,58 @@ public class RunApp {
     
     public static void main(String[] args){
         
+        OptionParser p = new OptionParser();
+        
+        p.addOption("-t", "4", "number of threads");
+        p.addRequired("-o", "output file");
+        
+        p.parse(args);
+        
         //String file = "/Users/gavalian/Work/DataSpace/decoded/clas_006595.evio.00625-00629_DC.hipo";
         String file = "/Users/tyson/data_repo/trigger_data/rgd/018326/run_18326_3_wAIBanks.h5";
         //String file = "/Users/tyson/data_repo/trigger_data/sims/claspyth_train/clasdis_62.hipo";
+        
+        String output = p.getOption("-o").stringValue();
+        int nt = p.getOption("-t").intValue();
+        file = p.getInputList().get(0);
         HipoReader r = new HipoReader(file);
         
-        HipoWriter w = HipoWriter.create("w.h5", r);
+        HipoWriter w = HipoWriter.create(output, r);
         
         DataActorStream stream = new DataActorStream();
         
         stream.setSource(r).setSync(w);
 
-        String pathToClusterFinder = "etc/networks/clusterfinder/cf";
-        String pathToElPID = "etc/networks/ElPID/ElPID";
+        String pathToClusterFinder = "etc/networks_rga/clusterfinder/cf";
+        String pathToElPID = "etc/networks_rga/ElPID/ElPID";
         double threshold=0.1;        
         stream.setSource(r).setSync(w);
         
                 
         ConverterWorker   convert = new ConverterWorker();
+        
+        Clas12DecoderService decoder = new Clas12DecoderService();
+        Clas12FitterService   fitter = new Clas12FitterService();
+        Clas12TranslateService trans = new Clas12TranslateService();
+        decoder.keepEvio = true;
+        
+        trans.setKeepEvio(true);
         DriftChamberWorker  dcwrk = new DriftChamberWorker();
         TrackFinderWorker  finder = new TrackFinderWorker();
+        finder.finder.DO_5_SUPERLAYER = false;
+        
         ClusterFinderWorkerECAL  ecalfinder = new ClusterFinderWorkerECAL(pathToClusterFinder);
         ClusterFinderWorkerFTOF  ftoffinder = new ClusterFinderWorkerFTOF(pathToClusterFinder);
         ElPIDWorker elPID  = new ElPIDWorker(pathToElPID,threshold);
         
         finder.initNetworks();
-        
-        List<DataWorker>  workers = Arrays.asList(convert,dcwrk, finder,ecalfinder,ftoffinder,elPID);
+        Hipo2Hipo4Worker     h2h4 = new Hipo2Hipo4Worker(64*1024);
+        //List<DataWorker>  workers = Arrays.asList(decoder,fitter,trans,dcwrk, finder,ecalfinder,ftoffinder,elPID,h2h4);
+        List<DataWorker>  workers = Arrays.asList(decoder,fitter,trans,dcwrk, finder,ecalfinder,ftoffinder,elPID,h2h4);
         //List<DataWorker>  workers = Arrays.asList(convert,dcwrk, finder);
         
-        List<DataActor>   actors = RunApp.createActors(6, 128, workers);
-        
+        List<DataActor>   actors = RunApp.createActors(nt, 128, workers);
+        actors.get(0).setBenchmark(1);
         stream.addActor(actors);//.addActor(convert2);//.addActor(convert3).addActor(convert4);
         
         stream.run();
